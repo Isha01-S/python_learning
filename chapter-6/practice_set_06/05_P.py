9a7a736dbd099b511c486c976e368ca5193b2fae
Isha01-S <ishamca01@gmail.com>
2025-08-18 20:01:39 +0530

    added problem 5

---
+++ chapter-6/practice_set_06/05_P.py
@@ -0,0 +1,9 @@
+"""5. Write a program which finds out whether a given name is present in a list or not."""
+l=["isha","divya","diya","shivam"]
+name= input("Enter the name : ")
+
+if name in l:
+    print("Present")
+
+else:
+    print("Not present")
