8d67d5e4312161b9210cd03827810d6c8fc82c31
Isha01-S <ishamca01@gmail.com>
2025-08-19 17:38:55 +0530

    solved problem1

---
+++ chapter-7/practice_set_07/01_P.py
@@ -0,0 +1,6 @@
+'''1. Write a program to print multiplication table of a given number using for loop.'''
+i=1
+n=int(input("Enter the number : "))
+while i<=10:
+    print(f"{n} x {i} = {n*i} ")
+    i+=1
