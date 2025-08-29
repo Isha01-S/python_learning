0d2f8b986fdd896c3977e8e6a31b082e1e976724
Isha01-S <ishamca01@gmail.com>
2025-08-29 22:26:16 +0530

    added three problems of file input and output

---
+++ chapter-9(file_input_output)/practice_set_09/02_P.py
@@ -0,0 +1,26 @@
+'''The game() function in a program lets a user play a game and returns the score
+as an integer. You need to read a file ‘Hi-score.txt’ which is either blank or
+contains the previous Hi-score. You need to write a program to update the Hiscore whenever the game() function breaks the Hi-score.'''
+import random
+def game():
+    print("You are playing the number game!")
+    score = random.randint(1,60)
+
+    #fetch the highscore
+    with open('highscore.txt') as f:
+        highscore=f.read()
+        if (highscore!=""):
+            highscore=int(highscore)
+        else:
+            highscore=0
+    print(f"Your score is {score}")
+
+    if (score > highscore):
+        #write this high score in the file
+        #fetch the highscore
+        with open('highscore.txt','w') as f:
+            highscore=f.write(str(score))
+        
+    return score
+
+game()
